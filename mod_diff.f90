27cf8fb9f7bd17b3fa3abda13113d4d2cbd501ef
mrosesyntax <mroses@syntaxsdg.com>
2026-02-05 11:39:43 -0600

    debug ch4

--- mod_diff.f90
+++ mod_diff.f90
@@ -2,9 +2,12 @@ module mod_diff
     use iso_fortran_env, only : int32, real32
     implicit none
     
+    private
+    public :: diff_upwind, diff_centered
 contains
 
-    function diff(x) result (dx)
+
+    pure function diff_upwind(x) result(dx)
         real, intent(in) :: x(:)  ! Tells compiler this is array of some length
         real :: dx(size(x))
         integer :: im
@@ -13,7 +16,23 @@ contains
         dx(1) = x(1) - x(im)
         ! Array oriented syntax here
         dx(2:im) = x(2:im) - x(1:im-1) 
-    end function diff
+    end function diff_upwind
+
+    pure function diff_centered(x) result(dx)
+        real, intent(in) :: x(:)  ! Tells compiler this is array of some length
+        real :: dx(size(x))
+        integer :: im
+
+        im = size(x)
+        ! Centered diff
+        dx(1) = x(2) - x(im)
+        dx(im) = x(1) - x(im-1)
+        dx(2:im-1) = x(3:im) - x(1:im-2)
+
+        ! Centered diff requires scaling
+        dx = 0.5*dx
+      
+    end function diff_centered
 
     
 end module mod_diff
--- tsunami.f90
+++ tsunami.f90
@@ -1,34 +1,38 @@
 program tsunami
-    use mod_diff, only: diff
+    use iso_fortran_env, only: int32, real32
+    use mod_diff, only: diff => diff_centered
     use mod_initial, only: set_gaussian
+    
     implicit none
 
-    integer :: n
-    integer, parameter :: grid_size = 100
-    integer, parameter :: num_time_steps = 100
+    integer(int32) :: n
+    integer(int32), parameter :: grid_size = 100
+    integer(int32), parameter :: num_time_steps = 5000
 
-    real, parameter :: dt = 1 ! time step [s]
-    real, parameter :: dx = 1 ! grid spacing [m]
-    real, parameter :: c = 1 ! phase speed [m/s]
+    real(real32), parameter :: dt = 0.02 ! time step [s]
+    real(real32), parameter :: dx = 1 ! grid spacing [m]
+    real(real32), parameter :: g = 9.8 ! gravity [m/s]
+    real(real32), parameter :: hmean = 10 ! mean depth of water [m]
 
-    real :: h(grid_size)
-    real :: dh(grid_size)
 
-    integer, parameter :: icenter = 25
-    real, parameter :: decay = 0.02
+    real(real32) :: h(grid_size)
+    real(real32) :: u(grid_size)
+
+    integer(int32), parameter :: icenter = 25
+    real(real32), parameter :: decay = 0.02
 
     if (grid_size <= 0) stop "Grid Size <= 0"
     if (dt <= 0) stop "dt <= 0"
     if (dx <= 0) stop "dx <= 0"
-    if (c <= 0) stop "c < 0"
 
     ! Set initial water height
     call set_gaussian(h, icenter, decay)
 
     print *, 0, h
     time_loop: do n = 1, num_time_steps        
-        dh = diff(h)
-        h = h - c * diff(h) / dx * dt
+        u = u - (u * diff(u) + g * diff(h)) / dx * dt
+        h = h - diff(u * (hmean + h)) / dx * dt
+       
         print *, n, h
     end do time_loop
 
